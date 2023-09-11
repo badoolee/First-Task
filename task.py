85c4a1a121834fb812936c79494e0e685eee9ccd
sulaimon taiwo <sulaimon.m1703582@st.futminna.edu.ng>
2023-09-11 10:17:21 +0100

    new correction

--- task.py
+++ task.py
@@ -10,65 +10,45 @@ api = Api(app)
 
 
 class slack_name(Resource):
-    name = "Sulaimon Taiwo Femi"
-    current_day = "Sunday"
-    current_time = datetime.now(pytz.utc)
-    track = "backend"
-    file = "https://github.com/badoolee/First-Task/blob/master/task.py"
-    code = "https://github.com/badoolee/First-Task"
-    status = 200
-
-    def __init__(self, name=name, current_day=current_day, 
-                 current_time=current_time, track=track, file=file, code=code, status=status):
-        self.name = name
-        self.current_day = current_day
-        self.current_time = current_time
-        self.track = track
-        self.file = file
-        self.code = code
-        self.status = status
     
     def get(self):
-        
+        name = "Sulaimon Taiwo Femi"
+        current_day = "Sunday"
+        current_time = datetime.now(pytz.utc)
+        track = "backend"
+        file = "https://github.com/badoolee/First-Task/blob/master/task.py"
+        code = "https://github.com/badoolee/First-Task"
+        status = 200
+
         return {
-            "name": self.name,
-            "current_day": self.current_day,
-            "current_time": str(self.current_time),
-            "track": self.track,
-            "file": self.file,
-            "code": self.code,
-            "status": self.status
+            "name": name,
+            "current_day": current_day,
+            "current_time": str(current_time),
+            "track": track,
+            "file": file,
+            "code": code,
+            "status": status
         }
 
 class track(Resource):
-    name = "Sulaimon Taiwo Femi"
-    current_day = "Sunday"
-    current_time = datetime.now(pytz.utc)
-    track = "backend"
-    file = "file"
-    code = "code"
-    status = 200
-
-    def __init__(self, name=name, current_day=current_day, 
-                 current_time=current_time, track=track, file=file, code=code, status=status):
-        self.name = name
-        self.current_day = current_day
-        self.current_time = current_time
-        self.track = track
-        self.file = file
-        self.code = code
-        self.status = status
     
     def get(self):
-        
+        name = "Sulaimon Taiwo Femi"
+        current_day = "Sunday"
+        current_time = datetime.now(pytz.utc)
+        track = "backend"
+        file = "file"
+        code = "code"
+        status = 200
+
         return {
-            "name": self.name,
-            "current_day": self.current_day,
-            "current_time": str(self.current_time),
-            "track": self.track,
-            "file": self.file,
-            "code": self.code,
-            "status": self.status
+            "name": name,
+            "current_day": current_day,
+            "current_time": str(current_time),
+            "track": track,
+            "file": file,
+            "code": code,
+            "status": status
         }
     
         
